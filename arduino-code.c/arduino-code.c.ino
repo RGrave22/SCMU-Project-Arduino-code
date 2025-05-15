04439a73d14c8fef324e3c389ed39208b6b52038
RGrave22 <r.grave@campus.fct.unl.pt>
2025-05-15 12:12:58 +0100

    joined the mqtt with the firebase code, needs to be tested

--- arduino-code.c/arduino-code.c.ino
+++ arduino-code.c/arduino-code.c.ino
@@ -1,32 +1,53 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <Firebase_ESP_Client.h>
+#include <PubSubClient.h>
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
 //Wifi credentials
-#define WIFI_SSID "OpenFCT"
-#define WIFI_PASSWORD ""
+#define WIFI_SSID "Grave"
+#define WIFI_PASSWORD "grave123"
 
 //Firebase credentials
 #define API_KEY "AIzaSyDB8cIkBU5lxgIqYjr7YKDWvtlyf5SIeQ4"
 #define DATABASE_URL "https://scmu-7d9e7-default-rtdb.europe-west1.firebasedatabase.app/" 
 
+//MQTT credentias
+#define MQTT_BROKER_ADRESS "broker.hivemq.com"
+#define MQTT_CLIENT_NAME "botaniq_client"
+#define MQTT_PORT 1883
+
+/* VERIFY IF THE WORKS WITH THE DEFINES ABOVE, IF NOT DELETE THE DEFINES AND UNCOMMENT THIS
+const char *MQTT_BROKER_ADRESS = "broker.hivemq.com";
+const uint16_t MQTT_PORT = 1883;
+const char *MQTT_CLIENT_NAME = "botaniq_client";
+*/
+
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
 unsigned long sendDataPrevMillis = 0;
 bool signupOK = false;
 
-void setup() {
+void setup() 
+{
   Serial.begin(115200);
 
+  //Setups
   setupWifi();
   setupFirebase();
+  setupMQTT();
 }
 
-void loop() {
+void loop() 
+{
+  HandleMQTT();
+
   if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 10000 || sendDataPrevMillis == 0)) {
     sendDataPrevMillis = millis();
 
@@ -46,10 +67,8 @@ void loop() {
 }
 
 
-
-void setupWifi() {
-
-  // Connect to Wi-Fi
+void setupWifi() 
+{
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
 
@@ -60,12 +79,10 @@ void setupWifi() {
 
   Serial.println();
   Serial.println("Connected with IP: " + WiFi.localIP().toString());
-
 }
 
-void setupFirebase(){
-
-  //Firebase
+void setupFirebase()
+{
   config.api_key = API_KEY;
   config.database_url = DATABASE_URL;
 
@@ -78,7 +95,62 @@ void setupFirebase(){
 
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
-
 }
+
+
+void setupMQTT()
+{
+    mqttClient.setServer(MQTT_BROKER_ADRESS, MQTT_PORT);
+    mqttClient.setCallback(OnMqttReceived);
+}
+
+void subscribeMQTT()
+{ //ADD MORE WHEN NEEDED
+    mqttClient.subscribe("cocomole");
+}
+
+void OnMqttReceived(char *topic, byte *payload, unsigned int length)
+{
+    Serial.print("Received on ");
+    Serial.print(topic);
+    Serial.print(": ");
+
+    String content = "";
+    for (size_t i = 0; i < length; i++)
+    {
+        content.concat((char)payload[i]);
+    }
+    Serial.print(content);
+    Serial.println();
+}
+
+void HandleMQTT()
+{
+    if (!mqttClient.connected())
+    {
+        ConnectMqtt();
+    }
+    mqttClient.loop();
+}
+
+void ConnectMQTT()
+{
+    Serial.print("Starting MQTT connection...");
+    if (mqttClient.connect(MQTT_CLIENT_NAME))
+    {
+        SuscribeMqtt();
+        mqttClient.publish("connected","hello/world");
+    }
+    else
+    {
+        Serial.print("Failed MQTT connection, rc=");
+        Serial.print(mqttClient.state());
+        Serial.println(" try again in 5 seconds");
+
+        delay(5000);
+    }
+}
+
+
 
 
